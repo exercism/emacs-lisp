2a4ccfdf285aed10179d9eefda9947ad8a32c698
Jason Lewis <jason@decomplecting.org>
2015-07-25 01:29:34 -0400

    Add Leap exercise

---
+++ leap/example.el
@@ -0,0 +1,14 @@
+;;; leap.el --- Leap exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun leap-year-p (year)
+  "Determine if YEAR is a leap year."
+  (and (= 0 (mod year 4))
+       (or (not (= 0 (mod year 100)))
+           (= 0 (mod year 400)))))
+
+(provide 'leap)
+;;; leap.el ends here
---
+++ leap/leap.el
@@ -0,0 +1,9 @@
+;;; leap.el --- Leap exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'leap)
+;;; leap.el ends here
