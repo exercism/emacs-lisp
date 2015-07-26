98e81b24ed2949bf37be86f91a35b9981af4be86
Jason Lewis <jason@decomplecting.org>
2015-07-26 03:58:40 -0400

    Oops forgot to move example to example.el

--- allergies/allergies.el
+++ allergies/allergies.el
@@ -4,50 +4,7 @@
 
 ;;; Code:
 
-(require 'cl)
 
-(defvar *allergens-scores*
-  '(("eggs" 1)
-    ("peanuts" 2)
-    ("shellfish" 4)
-    ("strawberries" 8)
-    ("tomatoes" 16)
-    ("chocolate" 32)
-    ("pollen" 64)
-    ("cats" 128)))
-
-(defun allergen (allergen-and-score)
-  "Return allergen from ALLERGEN-AND-SCORE."
-  (first allergen-and-score))
-
-(defun score (allergen-and-score)
-  "Return score from ALLERGEN-AND-SCORE."
-  (second allergen-and-score))
-
-(defun score-matches (allergen-and-score score)
-  "Determine if ALLERGEN-AND-SCORE entry match SCORE."
-  (not (zerop (logand (score allergen-and-score) score))))
-
-(defun find-all (check seq)
-  "Find each match for CHECK in SEQ."
-  (remove-if-not check seq))
-
-(defun allergen-list (score)
-  "List all allergens with a given SCORE."
-  (mapcar 'allergen
-          (find-all
-           '(lambda (as) (score-matches as score))
-           *allergens-scores*)))
-
-(defun allergic-to-p (score allergen)
-  "Allergic-to predicate based on SCORE and ALLERGEN."
-  (score-matches (cl-assoc allergen *allergens-scores* :test 'string=)
-                 score))
-
-
-(let ((score 1))
- (find-all
-  '(lambda (as) (score-matches as score)) *allergens-scores*))
 
 (provide 'allergies)
 ;;; allergies.el ends here
