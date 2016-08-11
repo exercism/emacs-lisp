9b94d4f0cbf22c753edeafe26165325288dad892
C. Paul Bond <paul.bond@threewide.com>
2016-08-11 18:41:44 -0400

    Added word-count exercise.

---
+++ exercises/word-count/example.el
@@ -0,0 +1,18 @@
+;;; word-count.el --- word-count Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun word-count (sentence)
+  (let (count)
+    (dolist (i (split-string (downcase sentence) "[^a-z0-9]" t))
+      (let ((n (assoc i count)))
+        (if n (setcdr n (1+ (cdr n)))
+          (push (cons i 1) count))))
+    count))
+
+
+(provide 'word-count)
+;;; word-count.el ends here
