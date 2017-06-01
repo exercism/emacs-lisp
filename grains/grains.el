ae7be9d71c88bad52284bd355207e63112c43de2
Eric Bailey <eric@ericb.me>
2017-06-01 02:52:40 -0500

    Adopt @wobh's suggestions

--- grains/grains.el
+++ grains/grains.el
@@ -8,11 +8,11 @@
 
 (defun square (n)
   "Find the number of grains on square N."
-  (expt 2 (- n 1)))
+  (string-to-number (calc-eval (format "2^(%d - 1)" n))))
 
 (defun total ()
   "Find the total number of grains on the board."
-  (reduce #'+ (mapcar #'square (number-sequence 1 64))))
+  (string-to-number (calc-eval "2^64 - 1")))
 
 (provide 'grains)
 ;;; grains.el ends here
