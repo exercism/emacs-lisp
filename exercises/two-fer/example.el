fbd72a49ed7e57fa5e23ac9a673e0df797c1023e
Benjamin Reynolds <breyno127@gmail.com>
2018-04-29 10:56:26 -0500

    Implement two-fer exercise
    In the upstream decision to simplify the hello-world exercise, a new exercise
    was propsed and accepted to replace it. This commit introduces that exercise

---
+++ exercises/two-fer/example.el
@@ -0,0 +1,13 @@
+;;; two-fer.el --- Two-fer Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun two-fer (&optional name)
+  (let ((player (or name "you")))
+    (concat "One for " player ", one for me.")))
+
+(provide 'two-fer)
+
+;;; two-fer.el ends here
