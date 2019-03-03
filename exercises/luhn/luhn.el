1ad8408d1e827420de56d96a85bf4b0ff575546b
lispy-dobby <yilkalargawworkneh@gmail.com>
2019-03-03 15:01:22 +0000

    corrected luhn.el to just provide a template

--- exercises/luhn/luhn.el
+++ exercises/luhn/luhn.el
@@ -1,30 +1,11 @@
-(require 'seq)
-(defun luhn-p (dastring)
-  "Check if an input string DASTRING is valid using lhun algorithm."
-  (let ((str (replace-regexp-in-string " " "" dastring)))
-    (if (string-match-p "[^0-9 ]" str)
-	(error "String contains invalid character")
-      (progn
-	(if (<= (length str) 1)
-	    nil
-	  (let ((digit-list (reverse (mapcar #'(lambda (x) (- x 48))
-					     (string-to-list (replace-regexp-in-string " " "" str))))))
-	    (zerop (mod (apply #'+ (seq-map-indexed
-				    (lambda (elt idx) (if (oddp idx)
-							  (if (> (* 2 elt) 9)
-							      (- (* 2 elt) 9)
-							    (* 2 elt))
-							elt))
-				    digit-list))
-			10))))))))
+;;; luhn.el --- Luhn exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
 
 
 
 
-;;  (mapcar #'luhn '("49927398716" "49927398717" "1234567812345678" "1234567812345670"))
-
-;; output
-
-;; (t nil nil t)
-
-
+(provide 'luhn)
+;;; luhn.el ends here
