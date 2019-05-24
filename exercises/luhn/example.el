5b65a8d2764008f3880d3acb4c87c67a085f581c
Sean Allred <code@seanallred.com>
2019-05-23 22:24:30 -0500

    Whitespace/indentation changes

--- exercises/luhn/example.el
+++ exercises/luhn/example.el
@@ -15,15 +15,17 @@
 	  nil
 	(let* ((digit-list (reverse (mapcar (lambda (x) (- x 48))
 				            (string-to-list str))))
-	       (digit-list-with-index (cl-pairlis (number-sequence 0 (- (length digit-list) 1)) digit-list)))
-
-	    (zerop (mod (apply #'+ (mapcar (lambda (x) (if (equal 1 (mod (car x) 2))
-					    (if (> (* 2 (cdr x)) 9)
-						(- (* 2 (cdr x)) 9)
-					      (* 2 (cdr x)))
-					    (cdr x)))
-					   digit-list-with-index))
-			10))))))))
+	       (digit-list-with-index (cl-pairlis (number-sequence 0 (- (length digit-list) 1))
+                                                  digit-list)))
+	  (zerop (mod (apply #'+
+                             (mapcar (lambda (x)
+                                       (if (equal 1 (mod (car x) 2))
+				           (if (> (* 2 (cdr x)) 9)
+				               (- (* 2 (cdr x)) 9)
+				             (* 2 (cdr x)))
+				         (cdr x)))
+				     digit-list-with-index))
+		      10)))))))
 
 (provide 'luhn)
 ;;; luhn.el ends here
