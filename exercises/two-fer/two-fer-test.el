0e7dd72158a74a7db2dc8acccdddd86d45db3030
Sean Allred <code@seanallred.com>
2019-05-25 23:08:26 -0500

    Use `string=' instead of `equal' in tests
    Set a good example.

--- exercises/two-fer/two-fer-test.el
+++ exercises/two-fer/two-fer-test.el
@@ -8,13 +8,13 @@
 (load-file "two-fer.el")
 
 (ert-deftest no-name-given ()
-  (should (equal (two-fer) "One for you, one for me.")))
+  (should (string= (two-fer) "One for you, one for me.")))
 
 (ert-deftest a-name-given ()
-  (should (equal (two-fer "Alice") "One for Alice, one for me.")))
+  (should (string= (two-fer "Alice") "One for Alice, one for me.")))
 
 (ert-deftest another-name-given ()
-  (should (equal (two-fer "Bob") "One for Bob, one for me.")))
+  (should (string= (two-fer "Bob") "One for Bob, one for me.")))
 
 (provide 'two-fer-test)
 
