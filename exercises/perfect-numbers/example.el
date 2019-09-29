9b9b1787dbc809964990027c5f7da5a850bf49c1
guy <strings.stringandstrings@gmail.com>
2019-09-28 21:56:03 -0400

    update tests and example to pass

--- exercises/perfect-numbers/example.el
+++ exercises/perfect-numbers/example.el
@@ -3,26 +3,27 @@
 ;;; Commentary:
 
 ;;; Code:
+(require 'seq)
 
-(require 'cl-lib)
+(defun classify (number)
+  "Classify NUMBER as perfect, abundant, or deficient."
+  (when (< number 1)
+    (error "%s" "Classification is only possible for natural numbers"))
+  (let ((aliquot-sum (calculate-aliquot-sum number)))
+    (cond ((> aliquot-sum number) 'abundant)
+          ((= aliquot-sum number) 'perfect)
+          ((< aliquot-sum number) 'deficient))))
 
+(defun calculate-aliquot-sum (number)
+  "The aliquot sum of NUMBER is the sum of its factors."
+  (let ((factors (seq-filter
+                  (lambda (x) (divides-p x number))
+                  (number-sequence 1 (/ number 2)))))
+    (seq-reduce #'+ factors 0)))
 
-(defun divisors (n)
-  (cl-loop for i from 1 to (truncate (sqrt n))
-           if (= (% n i) 0) collect i
-           and if (not (= i (/ n i))) collect (/ n i)))
-
-
-(defun sum-divisors (n)
-  ;; Remove n from the list and add'em up.
-  (apply #'+ (cdr (sort (divisors n) '>))))
-
-
-(defun perfect-numbers (n)
-  (cl-loop for i from 2 to n by 2
-           when (= i (sum-divisors i))
-           collect i))
-
+(defun divides-p (factor multiple)
+  "True if FACTOR divides MULTIPLE."
+  (zerop (mod multiple factor)))
 
 (provide 'perfect-numbers)
 ;;; perfect-numbers.el ends here
