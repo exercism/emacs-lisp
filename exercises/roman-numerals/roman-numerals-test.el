26f509860a00606a8efb293f34f1a10a48680fcc
Bakhtiyor Aripov <bakhti@gmail.com>
2019-10-14 16:03:02 +0200

    Split a single test in roman-numerals into multiple separate ones

--- exercises/roman-numerals/roman-numerals-test.el
+++ exercises/roman-numerals/roman-numerals-test.el
@@ -6,24 +6,58 @@
 
 (load-file "roman-numerals.el")
 
-(ert-deftest roman-numerals-test ()
-  (should (equal (to-roman 1) "I"))
-  (should (equal (to-roman 2) "II"))
-  (should (equal (to-roman 3) "III"))
-  (should (equal (to-roman 4) "IV"))
-  (should (equal (to-roman 5) "V"))
-  (should (equal (to-roman 6) "VI"))
-  (should (equal (to-roman 9) "IX"))
-  (should (equal (to-roman 27) "XXVII"))
-  (should (equal (to-roman 48) "XLVIII"))
-  (should (equal (to-roman 59) "LIX"))
-  (should (equal (to-roman 93) "XCIII"))
-  (should (equal (to-roman 141) "CXLI"))
-  (should (equal (to-roman 163) "CLXIII"))
-  (should (equal (to-roman 402) "CDII"))
-  (should (equal (to-roman 575) "DLXXV"))
-  (should (equal (to-roman 911) "CMXI"))
-  (should (equal (to-roman 1024) "MXXIV"))
+(ert-deftest to-roman-1 ()
+  (should (equal (to-roman 1) "I")))
+
+(ert-deftest to-roman-2 ()
+  (should (equal (to-roman 2) "II")))
+
+(ert-deftest to-roman-3 ()
+  (should (equal (to-roman 3) "III")))
+
+(ert-deftest to-roman-4 ()
+  (should (equal (to-roman 4) "IV")))
+
+(ert-deftest to-roman-5 ()
+  (should (equal (to-roman 5) "V")))
+
+(ert-deftest to-roman-6 ()
+  (should (equal (to-roman 6) "VI")))
+
+(ert-deftest to-roman-9 ()
+  (should (equal (to-roman 9) "IX")))
+
+(ert-deftest to-roman-27 ()
+  (should (equal (to-roman 27) "XXVII")))
+
+(ert-deftest to-roman-48 ()
+  (should (equal (to-roman 48) "XLVIII")))
+
+(ert-deftest to-roman-59 ()
+  (should (equal (to-roman 59) "LIX")))
+
+(ert-deftest to-roman-93 ()
+  (should (equal (to-roman 93) "XCIII")))
+
+(ert-deftest to-roman-141 ()
+  (should (equal (to-roman 141) "CXLI")))
+
+(ert-deftest to-roman-163 ()
+  (should (equal (to-roman 163) "CLXIII")))
+
+(ert-deftest to-roman-402 ()
+  (should (equal (to-roman 402) "CDII")))
+
+(ert-deftest to-roman-575 ()
+  (should (equal (to-roman 575) "DLXXV")))
+
+(ert-deftest to-roman-911 ()
+  (should (equal (to-roman 911) "CMXI")))
+
+(ert-deftest to-roman-1024 ()
+  (should (equal (to-roman 1024) "MXXIV")))
+
+(ert-deftest to-roman-3000 ()
   (should (equal (to-roman 3000) "MMM")))
 
 (provide 'roman-numerals)
