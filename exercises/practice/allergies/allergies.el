926a72e554ffc08e9c1b6028311c9cdc7883c235
nitincodery <54881266+nitincodery@users.noreply.github.com>
2022-09-28 23:39:22 +0545

    Update allergies.el

--- exercises/practice/allergies/allergies.el
+++ exercises/practice/allergies/allergies.el
@@ -2,9 +2,14 @@
 
 ;;; Commentary:
 
+(defun allergen-list (score)
 ;;; Code:
+)
 
-
+(defun allergic-to (score allergen)
+"Check if Allergic to allergen based on SCORE and ALLERGEN."
+;;; Code:
+)
 
 (provide 'allergies)
 ;;; allergies.el ends here
