8155386139dc843c0adba65880e7ab0e08b02f39
nitincodery <54881266+nitincodery@users.noreply.github.com>
2022-09-30 23:59:02 +0545

    Update difference-of-squares.el

--- exercises/practice/difference-of-squares/difference-of-squares.el
+++ exercises/practice/difference-of-squares/difference-of-squares.el
@@ -2,8 +2,17 @@
 
 ;;; Commentary:
 
+(defun sum-of-squares (n)
 ;;; Code:
+)
 
+(defun square-of-sum (n)
+;;; Code:
+)
+
+(defun difference (n)
+;;; Code:
+)
 
 (provide 'difference-of-squares)
 ;;; difference-of-squares.el ends here
