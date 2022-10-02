7e2b792300b3c3b3e326e7e56f0c5aa4db81df54
nitincodery <54881266+nitincodery@users.noreply.github.com>
2022-10-03 00:00:04 +0545

    Update robot-name.el

--- exercises/practice/robot-name/robot-name.el
+++ exercises/practice/robot-name/robot-name.el
@@ -6,10 +6,13 @@
 ;; to a new name. Instructions are in README.md
 ;;
 
+(defun robot-name (robot)
 ;;; Code:
+)
 
-
-
+(defun reset-robot (robot)
+;;; Code:
+)
 
 (provide 'robot-name)
 ;;; robot-name.el ends here
