703ec0d1a50644d7e3313a145aeb2957ac1f1c2d
nitincodery <54881266+nitincodery@users.noreply.github.com>
2022-10-02 23:53:35 +0545

    Update phone-number.el

--- exercises/practice/phone-number/phone-number.el
+++ exercises/practice/phone-number/phone-number.el
@@ -2,8 +2,17 @@
 
 ;;; Commentary:
 
+(defun numbers (num)
 ;;; Code:
+)
 
+(defun area-code (num)
+;;; Code:
+)
+
+(defun pprint (num)
+;;; Code:
+)
 
 (provide 'phone-number)
 ;;; phone-number.el ends here
