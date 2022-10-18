5b436fd45cdfcb8b2adc968a2b18db3c7ee16f69
fap <459631+fapdash@users.noreply.github.com>
2022-10-18 23:25:41 +0200

    Sync tests for 'leap' exercise with canonical tests
    Syncs the tests for 'leap' with the canonical tests from the
    problem-specifications.
    The `meta/tests.toml` is unchanged since all tests where already
    marked as implemented in there.
    
    closes #193

--- exercises/practice/leap/leap-test.el
+++ exercises/practice/leap/leap-test.el
@@ -7,20 +7,32 @@
 (load-file "leap.el")
 (declare-function leap-year-p "leap.el" (year))
 
-(ert-deftest vanilla-leap-year ()
+(ert-deftest year-not-divisible-by-4-in-common-year ()
+  (should-not (leap-year-p 2015)))
+
+(ert-deftest year-divisible-by-2-not-divisible-by-4-in-common-year ()
+  (should-not (leap-year-p 1970)))
+
+(ert-deftest year-divisible-by-4-not-divisible-by-100-in-leap-year ()
   (should (leap-year-p 1996)))
 
-(ert-deftest any-old-year ()
-  (should-not (leap-year-p 1997)))
+(ert-deftest year-divisible-by-4-and-5-is-still-a-leap-year ()
+  (should (leap-year-p 1960)))
 
-(ert-deftest non-leap-even-year ()
-  (should-not (leap-year-p 1998)))
+(ert-deftest year-divisible-by-100-not-divisible-by-400-in-common-year ()
+  (should-not (leap-year-p 2100)))
 
-(ert-deftest century ()
+(ert-deftest year-divisible-by-100-but-not-by-3-is-still-not-a-leap-year ()
   (should-not (leap-year-p 1900)))
 
-(ert-deftest exceptional-century ()
+(ert-deftest year-divisible-by-400-is-leap-year ()
   (should (leap-year-p 2000)))
+
+(ert-deftest year-divisible-by-400-but-not-by-125-is-still-leap-year ()
+  (should (leap-year-p 2400)))
+
+(ert-deftest year-divisible-by-200-not-divisible-by-400-in-common-year ()
+  (should-not (leap-year-p 1800)))
 
 (provide 'leap-test)
 ;;; leap-test.el ends here
