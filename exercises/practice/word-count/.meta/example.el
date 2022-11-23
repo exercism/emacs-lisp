9da78709a4ea19759b82764d1bd58311231c3b78
FAP <459631+fapdash@users.noreply.github.com>
2022-11-23 20:35:16 +0100

    Sync word-count exercise (#255)

--- exercises/practice/word-count/.meta/example.el
+++ exercises/practice/word-count/.meta/example.el
@@ -5,14 +5,19 @@
 ;;; Code:
 
 
+(defvar exercisms--word-regexp "\\(\\([a-zA-Z]+\\('[a-zA-Z]+\\)?\\)\\|[0-9]+\\)")
+
 (defun word-count (sentence)
-  (let ((count nil))
-    (dolist (word (split-string (downcase sentence) "[^a-z0-9']" t))
-      (let ((entry (assoc-string word count)))
-        (if entry
-            (setcdr entry (1+ (cdr entry)))
-          (push (cons word 1) count))))
-    count))
+  (let ((start 0)
+        (word-counts '()))
+    (save-match-data (while (string-match exercisms--word-regexp sentence start)
+                       (let ((match (downcase (match-string 1 sentence))))
+                         (if (assoc match word-counts)
+                             (setf (cdr (assoc match word-counts))
+                                   (1+ (assoc-default match word-counts 'equal 0)))
+                           (push `(,match . 1) word-counts)))
+                       (setq start (match-end 1))))
+    word-counts))
 
 
 (provide 'word-count)
