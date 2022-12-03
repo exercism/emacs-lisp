c132f5bb8066b345fe6878f2f0a8c294d676f1b1
fap <459631+fapdash@users.noreply.github.com>
2022-12-03 20:34:07 +0100

    Add Sublist exercise

---
+++ exercises/practice/sublist/sublist.el
@@ -0,0 +1,11 @@
+;;; sublist.el --- Sublist (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(defun list-classify (list1 list2)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(provide 'sublist)
+;;; sublist.el ends here
