1687cc36a627612747fcd11db05516300a27269e
fap <459631+fapdash@users.noreply.github.com>
2023-05-16 11:36:41 +0200

    Add queen-attack practice exercise

---
+++ exercises/practice/queen-attack/queen-attack.el
@@ -0,0 +1,18 @@
+;;; queen-attack.el --- Queen Attack (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun valid-position-p (queen)
+  (error
+   "Delete this S-Expression and write your own implementation"))
+
+(defun can-attack-p (white-queen black-queen)
+  (error
+   "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'queen-attack)
+;;; queen-attack.el ends here
