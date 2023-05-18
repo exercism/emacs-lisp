6ab3cfb9f4a551af3b0e14ac1b726f0c94ee5a47
fap <459631+fapdash@users.noreply.github.com>
2023-05-18 21:15:58 +0200

    Add binary-search practice exercise

---
+++ exercises/practice/binary-search/binary-search.el
@@ -0,0 +1,14 @@
+;;; binary-search.el --- Binary Search (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun find-binary (array value)
+  (error
+   "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'binary-search)
+;;; binary-search.el ends here
