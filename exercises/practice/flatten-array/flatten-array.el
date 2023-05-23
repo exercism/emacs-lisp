2d5fb6c19149addd7baeaca40ecd67f0e2e57cf0
FAP <459631+fapdash@users.noreply.github.com>
2023-05-24 00:48:46 +0200

    Add flatten-array practice exercise (#308)
    - reverse lists instead of arrays since they are the more common data
      structure in Emacs Lisp
    - use `list-flatten` function name to make it extra clear that lists are the
      expected input
    - use `list-flatten` function name instead of `flatten-list` to avoid overwriting
      built-in Emacs function (alias)

---
+++ exercises/practice/flatten-array/flatten-array.el
@@ -0,0 +1,14 @@
+;;; flatten-array.el --- Flatten Array (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun list-flatten (list)
+  (error
+   "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'flatten-array)
+;;; flatten-array.el ends here
