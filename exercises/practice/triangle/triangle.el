e4196b1d1f5a0aedc69236f8ab5597fe7c52cdf4
Eric Willigers <keiravillekode@gmail.com>
2023-06-06 19:26:27 +1000

    feat: Add exercise Triangle (closes #312)

---
+++ exercises/practice/triangle/triangle.el
@@ -0,0 +1,18 @@
+;;; triangle.el --- Triangle (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun equilateralp (sides)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun isoscelesp (sides)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun scalenep (sides)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(provide 'triangle)
+;;; triangle.el ends here
