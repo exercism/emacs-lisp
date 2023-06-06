b9b578dcde8492f9aeb9ef2ac91e446d59b20351
Eric Willigers <keiravillekode@gmail.com>
2023-06-06 11:13:02 +1000

    feat: Add exercise Collatz Conjecture (closes #310)

---
+++ exercises/practice/collatz-conjecture/collatz-conjecture.el
@@ -0,0 +1,12 @@
+;;; collatz-conjecture.el --- Collatz Conjecture (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(defun steps (number)
+  "Count the steps to reach 1 using the Collatz conjecture."
+  (error "Delete this S-Expression and write your own implementation"))
+
+(provide 'collatz-conjecture)
+;;; collatz-conjecture.el ends here
