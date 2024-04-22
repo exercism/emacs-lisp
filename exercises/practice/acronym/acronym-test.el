8466ff6e5ac2c39e6ade5418fca95e7c94778f7c
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-04-21 23:02:01 -0700

    Apply consistent formatting and string=

--- exercises/practice/acronym/acronym-test.el
+++ exercises/practice/acronym/acronym-test.el
@@ -4,36 +4,47 @@
 
 ;;; Code
 
+
 (load-file "acronym.el")
 (declare-function acronym "acronym.el" (phrase))
 
+
 (ert-deftest basic ()
-  (should (equal "PNG" (acronym "Portable Network Graphics"))))
+  (should (string= "PNG" (acronym "Portable Network Graphics"))))
+
 
 (ert-deftest lowercase-words ()
-  (should (equal "ROR" (acronym "Ruby on Rails"))))
+  (should (string= "ROR" (acronym "Ruby on Rails"))))
+
 
 (ert-deftest punctuation ()
-  (should (equal "FIFO" (acronym "First In, First Out"))))
+  (should (string= "FIFO" (acronym "First In, First Out"))))
+
 
 (ert-deftest all-caps-word ()
-  (should (equal "GIMP" (acronym "GNU Image Manipulation Program"))))
+  (should (string= "GIMP" (acronym "GNU Image Manipulation Program"))))
+
 
 (ert-deftest punctuation-without-white-space ()
-  (should (equal "CMOS" (acronym "Complementary metal-oxide semiconductor"))))
+  (should (string= "CMOS" (acronym "Complementary metal-oxide semiconductor"))))
+
 
 (ert-deftest very-long-abbreviation ()
-  (should (equal "ROTFLSHTMDCOALM"
+  (should (string= "ROTFLSHTMDCOALM"
                  (acronym "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me"))))
 
+
 (ert-deftest consecutive-delimiters ()
-  (should (equal "SIMUFTA" (acronym "Something - I made up from thin air"))))
+  (should (string= "SIMUFTA" (acronym "Something - I made up from thin air"))))
+
 
 (ert-deftest apostrophes ()
-  (should (equal "HC" (acronym "Halley's Comet"))))
+  (should (string= "HC" (acronym "Halley's Comet"))))
+
 
 (ert-deftest underscore-emphasis ()
-  (should (equal "TRNT" (acronym "The Road _Not_ Taken"))))
+  (should (string= "TRNT" (acronym "The Road _Not_ Taken"))))
+
 
 (provide 'acronym-test)
 ;;; acronym-test.el ends here
