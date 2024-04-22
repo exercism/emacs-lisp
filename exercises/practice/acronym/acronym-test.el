9fceafea0ee715c6bc0da3470861d6558860cd2a
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-04-22 16:20:27 -0700

    Swap result vs. expected

--- exercises/practice/acronym/acronym-test.el
+++ exercises/practice/acronym/acronym-test.el
@@ -10,40 +10,46 @@
 
 
 (ert-deftest basic ()
-  (should (string= "PNG" (acronym "Portable Network Graphics"))))
+  (should (string= (acronym "Portable Network Graphics")
+                   "PNG")))
 
 
 (ert-deftest lowercase-words ()
-  (should (string= "ROR" (acronym "Ruby on Rails"))))
+  (should (string= (acronym "Ruby on Rails")
+                   "ROR")))
 
 
 (ert-deftest punctuation ()
-  (should (string= "FIFO" (acronym "First In, First Out"))))
+  (should (string= (acronym "First In, First Out")
+                   "FIFO")))
 
 
 (ert-deftest all-caps-word ()
-  (should (string= "GIMP" (acronym "GNU Image Manipulation Program"))))
+  (should (string= (acronym "GNU Image Manipulation Program")
+                   "GIMP")))
 
 
 (ert-deftest punctuation-without-white-space ()
-  (should (string= "CMOS" (acronym "Complementary metal-oxide semiconductor"))))
+  (should (string= (acronym "Complementary metal-oxide semiconductor")
+                   "CMOS")))
 
 
 (ert-deftest very-long-abbreviation ()
-  (should (string= "ROTFLSHTMDCOALM"
-                   (acronym "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me"))))
+  (should (string= (acronym "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me")
+                   "ROTFLSHTMDCOALM")))
 
 
 (ert-deftest consecutive-delimiters ()
-  (should (string= "SIMUFTA" (acronym "Something - I made up from thin air"))))
+  (should (string= (acronym "Something - I made up from thin air")
+                   "SIMUFTA")))
 
 
 (ert-deftest apostrophes ()
-  (should (string= "HC" (acronym "Halley's Comet"))))
+  (should (string= (acronym "Halley's Comet") "HC")))
 
 
 (ert-deftest underscore-emphasis ()
-  (should (string= "TRNT" (acronym "The Road _Not_ Taken"))))
+  (should (string= (acronym "The Road _Not_ Taken") "TRNT")))
 
 
 (provide 'acronym-test)
