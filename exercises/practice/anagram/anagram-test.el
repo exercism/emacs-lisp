723256f63d06706cbd3ceb76bdffa92c224a1eb0
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-04-22 16:12:11 -0700

    Swap expected vs result order

--- exercises/practice/anagram/anagram-test.el
+++ exercises/practice/anagram/anagram-test.el
@@ -4,101 +4,91 @@
 
 ;;; Code:
 
+
 (load-file "anagram.el")
 (declare-function anagrams-for "anagram.el" (subject candidates))
 
+
 (ert-deftest no-matches ()
-  (should (equal '() (anagrams-for 
-                      "diaper"
-                      '("hello" "world" "zombies" "pants")))))
+  (should (equal (anagrams-for "diaper" '("hello" "world" "zombies" "pants"))
+                 '())))
 
 
 (ert-deftest detects-two-anagrams ()
-  (should (equal '("lemons" "melons")
-                 (anagrams-for
-                  "solemn"
-                  '("lemons" "cherry" "melons")))))
+  (should (equal (anagrams-for "solemn" '("lemons" "cherry" "melons"))
+                 '("lemons" "melons"))))
 
 
 (ert-deftest does-not-detect-anagram-subsets ()
-  (should (equal '() (anagrams-for "good" '("dog" "goody")))))
+  (should (equal (anagrams-for "good" '("dog" "goody"))
+                 '())))
 
 
 (ert-deftest detect-anagram ()
-  (should (equal '("inlets")
-                 (anagrams-for
-                  "listen"
-                  '("enlists" "google" "inlets" "banana")))))
+  (should (equal (anagrams-for "listen" '("enlists" "google" "inlets" "banana"))
+                 '("inlets"))))
 
 
 (ert-deftest detects-three-anagrams ()
-  (should (equal '("gallery" "regally" "largely")
-                 (anagrams-for 
-                  "allergy"
-                  '("gallery" "ballerina" "regally" "clergy" "largely" "leading")))))
+  (should (equal (anagrams-for "allergy"
+                               '("gallery" "ballerina" "regally" "clergy" "largely" "leading"))
+                 '("gallery" "regally" "largely"))))
 
 
 (ert-deftest detects-multiple-anagrams-with-different-case ()
-  (should (equal '("Eons" "ONES")
-                  (anagrams-for
-                    "nose"
-                    '("Eons" "ONES")))))
+  (should (equal (anagrams-for "nose" '("Eons" "ONES"))
+                 '("Eons" "ONES"))))
 
 
 (ert-deftest does-not-detect-non-anagram-with-identical-checksum ()
-  (should (equal '() (anagrams-for "mass" '("last")))))
+  (should (equal (anagrams-for "mass" '("last"))
+          '())))
 
 
 (ert-deftest detects-anagrams-case-insensitively ()
-  (should (equal '("Carthorse")
-                  (anagrams-for
-                   "Orchestra"
-                   '("cashregister" "Carthorse" "radishes")))))
+  (should (equal (anagrams-for "Orchestra" '("cashregister" "Carthorse" "radishes"))
+                 '("Carthorse"))))
 
 
 (ert-deftest detects-anagrams-using-case-insensitive-subject ()
-  (should (equal '("carthorse")
-                  (anagrams-for
-                   "Orchestra"
-                   '("cashregister" "carthorse" "radishes")))))
+  (should (equal (anagrams-for "Orchestra" '("cashregister" "carthorse" "radishes"))
+                 '("carthorse"))))
 
 
 (ert-deftest detects-anagrams-using-case-insensitive-possible-matches ()
-  (should (equal '("Carthorse")
-                  (anagrams-for
-                   "orchestra"
-                   '("cashregister" "Carthorse" "radishes")))))
+  (should (equal (anagrams-for "orchestra" '("cashregister" "Carthorse" "radishes"))
+                 '("Carthorse"))))
 
 
 (ert-deftest does-not-detect-anagram-if-original-word-is-repeated ()
-  (should (equal '() (anagrams-for "go" '("goGoGO")))))
+  (should (equal (anagrams-for "go" '("goGoGO"))
+                 '())))
 
 
 (ert-deftest anagrams-must-use-all-letters-exactly-once ()
-  (should (equal '() (anagrams-for "tapper" '("patter")))))
+  (should (equal (anagrams-for "tapper" '("patter"))
+                 '())))
 
 
 (ert-deftest word-is-not-own-anagram ()
-  (should (equal '() (anagrams-for "BANANA" '("BANANA")))))
+  (should (equal (anagrams-for "BANANA" '("BANANA"))
+                 '())))
 
 
 (ert-deftest word-is-not-own-anagram-if-letter-case-is-partially-different ()
-  (should (equal '()
-                 (anagrams-for
-                  "BANANA"
-                  '("Banana")))))
+  (should (equal (anagrams-for "BANANA" '("Banana"))
+                 '())))
+
 
 (ert-deftest word-is-not-own-anagram-if-letter-case-is-completely-different ()
-  (should (equal '()
-                 (anagrams-for
-                  "BANANA"
-                  '("banana")))))
+  (should (equal (anagrams-for "BANANA" '("banana"))
+                 '())))
+
 
 (ert-deftest words-other-than-themselves-can-be-anagrams ()
-  (should (equal '("Silent")
-                 (anagrams-for
-                  "LISTEN"
-                  '("LISTEN" "Silent")))))
+  (should (equal (anagrams-for "LISTEN" '("LISTEN" "Silent"))
+                 '("Silent"))))
+
 
 (provide 'anagram-test)
 ;;; anagram-test.el ends here
