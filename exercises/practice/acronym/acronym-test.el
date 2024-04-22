bad6f11845d35fd9ce1363f0fd5b1f26bf11100b
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-04-21 21:40:45 -0700

    Sync acronym tests

--- exercises/practice/acronym/acronym-test.el
+++ exercises/practice/acronym/acronym-test.el
@@ -16,14 +16,24 @@
 (ert-deftest punctuation ()
   (should (equal "FIFO" (acronym "First In, First Out"))))
 
-(ert-deftest all-caps-words ()
-  (should (equal "PHP" (acronym "PHP: Hypertext Preprocessor"))))
-
-(ert-deftest non-acronym-all-caps-word ()
+(ert-deftest all-caps-word ()
   (should (equal "GIMP" (acronym "GNU Image Manipulation Program"))))
 
-(ert-deftest hyphenated ()
+(ert-deftest punctuation-without-white-space ()
   (should (equal "CMOS" (acronym "Complementary metal-oxide semiconductor"))))
+
+(ert-deftest very-long-abbreviation ()
+  (should (equal "ROTFLSHTMDCOALM"
+                 (acronym "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me"))))
+
+(ert-deftest consecutive-delimiters ()
+  (should (equal "SIMUFTA" (acronym "Something - I made up from thin air"))))
+
+(ert-deftest apostrophes ()
+  (should (equal "HC" (acronym "Halley's Comet"))))
+
+(ert-deftest underscore-emphasis ()
+  (should (equal "TRNT" (acronym "The Road _Not_ Taken"))))
 
 (provide 'acronym-test)
 ;;; acronym-test.el ends here
