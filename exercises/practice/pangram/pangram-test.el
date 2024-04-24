177d2fe98b2d0973f3f4a93f6d1d53687e3ee01b
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-04-23 23:38:01 -0700

    Sync pangram tests and update stub (#358)

--- exercises/practice/pangram/pangram-test.el
+++ exercises/practice/pangram/pangram-test.el
@@ -1,42 +1,53 @@
-;;; pagram-test.el --- Tests for Pangram (exercism)  -*- lexical-binding: t; -*-
+;;; pangram-test.el --- Tests for Pangram (exercism)  -*- lexical-binding: t; -*-
 
 ;;; Commentary:
-;; Common test data version: 1.3.0 d79e13e
 
 ;;; Code:
+
 
 (load-file "pangram.el")
 (declare-function pangramp "pangram.el" (phrase))
 
-(ert-deftest sentence-empty ()
-  (should (equal nil (pangramp ""))))
 
-(ert-deftest recognizes-a-perfect-lower-case-pangram ()
-  (should (equal t (pangramp "abcdefghijklmnopqrstuvwxyz"))))
+(ert-deftest empty-sentence ()
+  (should-not (pangramp "")))
 
-(ert-deftest  pangram-with-only-lower-case ()
-  (should (equal t (pangramp "the quick brown fox jumps over the lazy dog"))))
 
-(ert-deftest missing-character-x ()
-  (should (equal nil (pangramp "a quick movement of the enemy will jeopardize five gunboats"))))
+(ert-deftest perfect-lower-case ()
+  (should (pangramp "abcdefghijklmnopqrstuvwxyz")))
 
-(ert-deftest missing-another-character-eg-h ()
-  (should (equal nil (pangramp "five boxing wizards jump quickly at it"))))
 
-(ert-deftest  pangram-with-underscores ()
-  (should (equal t (pangramp "the_quick_brown_fox_jumps_over_the_lazy_dog"))))
+(ert-deftest only-lower-case ()
+  (should (pangramp "the quick brown fox jumps over the lazy dog")))
 
-(ert-deftest  pangram-with-numbers ()
-  (should (equal t (pangramp "the 1 quick brown fox jumps over the 2 lazy dogs"))))
 
-(ert-deftest  missing-letters-replaced-by-numbers ()
-  (should (equal nil (pangramp "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"))))
+(ert-deftest missing-the-letter-x ()
+  (should-not (pangramp "a quick movement of the enemy will jeopardize five gunboats")))
 
-(ert-deftest  pangram-with-mixed-case-and-punctuation ()
-  (should (equal t (pangramp "\"Five quacking Zephyrs jolt my wax bed.\""))))
 
-(ert-deftest  a-m-and-A-M-are-26-different-characters-but-not-a-pangram ()
-  (should (equal nil (pangramp "abcdefghijklm ABCDEFGHIJKLM"))))
+(ert-deftest missing-the-letter-h ()
+  (should-not (pangramp "five boxing wizards jump quickly at it")))
+
+
+(ert-deftest with-underscores ()
+  (should (pangramp "the_quick_brown_fox_jumps_over_the_lazy_dog")))
+
+
+(ert-deftest with-numbers ()
+  (should (pangramp "the 1 quick brown fox jumps over the 2 lazy dogs")))
+
+
+(ert-deftest missing-letters-replaced-by-numbers ()
+  (should-not (pangramp "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog")))
+
+
+(ert-deftest mixed-case-and-punctuation ()
+  (should (pangramp "\"Five quacking Zephyrs jolt my wax bed.\"")))
+
+
+(ert-deftest a-m-and-A-M-are-26-different-characters-but-not-a-pangram ()
+  (should-not (pangramp "abcdefghijklm ABCDEFGHIJKLM")))
+
 
 (provide 'pangram-test)
-;;; pagram-test.el ends here
+;;; pangram-test.el ends here
