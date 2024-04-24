1a5f8c64237fbc53b5efb6c90d4dc9a476508f8c
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-04-23 23:39:54 -0700

    Sync anagram tests (#352)
    * Sync anagram tests
    
    * Update stub formatting
    
    * Swap expected vs result order

--- exercises/practice/anagram/anagram.el
+++ exercises/practice/anagram/anagram.el
@@ -2,9 +2,12 @@
 
 ;;; Commentary:
 
-(defun anagrams-for (subject candidates)
 ;;; Code:
-)
+
+
+(defun anagrams-for (subject candidates)
+ (error "Delete this S-Expression and write your own implementation"))
+
 
 (provide 'anagram)
 ;;; anagram.el ends here
