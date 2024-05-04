b2f906aa9420014c20cb9d172b7feb54289d8345
Eric Willigers <keiravillekode@gmail.com>
2024-05-05 06:37:55 +1000

    should-not

--- exercises/practice/triangle/triangle-test.el
+++ exercises/practice/triangle/triangle-test.el
@@ -15,15 +15,15 @@
 
 
 (ert-deftest any-side-is-unequal ()
-  (should (not (equilateralp '(2 3 2)))))
+  (should-not (equilateralp '(2 3 2))))
 
 
 (ert-deftest no-sides-are-equal ()
-  (should (not (equilateralp '(5 4 6)))))
+  (should-not (equilateralp '(5 4 6))))
 
 
 (ert-deftest all-zero-sides-is-not-a-triangle ()
-  (should (not (equilateralp '(0 0 0)))))
+  (should-not (equilateralp '(0 0 0))))
 
 
 (ert-deftest sides-may-be-floats ()
@@ -47,19 +47,19 @@
 
 
 (ert-deftest no-sides-are-equal ()
-  (should (not (isoscelesp '(2 3 4)))))
+  (should-not (isoscelesp '(2 3 4))))
 
 
 (ert-deftest first-triangle-inequality-violation ()
-  (should (not (isoscelesp '(1 1 3)))))
+  (should-not (isoscelesp '(1 1 3))))
 
 
 (ert-deftest second-triangle-inequality-violation ()
-  (should (not (isoscelesp '(1 3 1)))))
+  (should-not (isoscelesp '(1 3 1))))
 
 
 (ert-deftest third-triangle-inequality-violation ()
-  (should (not (isoscelesp '(3 1 1)))))
+  (should-not (isoscelesp '(3 1 1))))
 
 
 (ert-deftest sides-may-be-floats ()
@@ -71,23 +71,23 @@
 
 
 (ert-deftest all-sides-are-equal ()
-  (should (not (scalenep '(4 4 4)))))
+  (should-not (scalenep '(4 4 4))))
 
 
 (ert-deftest first-and-second-sides-are-equal ()
-  (should (not (scalenep '(4 4 3)))))
+  (should-not (scalenep '(4 4 3))))
 
 
 (ert-deftest first-and-third-sides-are-equal ()
-  (should (not (scalenep '(3 4 3)))))
+  (should-not (scalenep '(3 4 3))))
 
 
 (ert-deftest second-and-third-sides-are-equal ()
-  (should (not (scalenep '(4 3 3)))))
+  (should-not (scalenep '(4 3 3))))
 
 
 (ert-deftest may-not-violate-triangle-inequality ()
-  (should (not (scalenep '(7 3 2)))))
+  (should-not (scalenep '(7 3 2))))
 
 
 (ert-deftest sides-may-be-floats ()
