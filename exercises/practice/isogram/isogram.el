a124e1146ab8ee4143afd735d7901338d56a6f2e
Eric Willigers <keiravillekode@gmail.com>
2024-05-05 17:10:55 +1000

    Add isogram exercise

---
+++ exercises/practice/isogram/isogram.el
@@ -0,0 +1,13 @@
+;;; isogram.el --- isogram (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun isogramp (phrase)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'isogram)
+;;; isogram.el ends here
