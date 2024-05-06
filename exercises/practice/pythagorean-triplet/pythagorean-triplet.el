5ae36af354a1aaa822ac3c2f7887c1dfb811b1ac
Eric Willigers <keiravillekode@gmail.com>
2024-05-06 22:56:47 +1000

    Add pythagorean-triplet exercise

---
+++ exercises/practice/pythagorean-triplet/pythagorean-triplet.el
@@ -0,0 +1,13 @@
+;;; pythagorean-triplet.el --- Pythagorean Triplet (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun triplets-with-sum (n)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'pythagorean-triplet)
+;;; pythagorean-triplet.el ends here
