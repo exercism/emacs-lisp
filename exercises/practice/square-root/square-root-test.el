682dd0f54980c86a31657e38370cc28e5d88503c
Eric Willigers <keiravillekode@gmail.com>
2024-05-08 09:18:06 +1000

    Compare numbers using =

--- exercises/practice/square-root/square-root-test.el
+++ exercises/practice/square-root/square-root-test.el
@@ -9,27 +9,27 @@
 
 
 (ert-deftest root-of-1 ()
-  (should (equal 1 (square-root 1))))
+  (should (= 1 (square-root 1))))
 
 
 (ert-deftest root-of-4 ()
-  (should (equal 2 (square-root 4))))
+  (should (= 2 (square-root 4))))
 
 
 (ert-deftest root-of-25 ()
-  (should (equal 5 (square-root 25))))
+  (should (= 5 (square-root 25))))
 
 
 (ert-deftest root-of-81 ()
-  (should (equal 9 (square-root 81))))
+  (should (= 9 (square-root 81))))
 
 
 (ert-deftest root-of-196 ()
-  (should (equal 14 (square-root 196))))
+  (should (= 14 (square-root 196))))
 
 
 (ert-deftest root-of-65025 ()
-  (should (equal 255 (square-root 65025))))
+  (should (= 255 (square-root 65025))))
 
 
 (provide 'square-root-test)
