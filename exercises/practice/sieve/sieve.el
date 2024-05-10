e5131342a2fb695ae0c8e913dd92e6420f46729f
Eric Willigers <keiravillekode@gmail.com>
2024-05-11 05:00:35 +1000

    Add sieve exercise (#376)
    * Add sieve exercise
    
    * reformat across multiple lines, set difficulty 3

---
+++ exercises/practice/sieve/sieve.el
@@ -0,0 +1,14 @@
+;;; sieve.el --- Sieve (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun primes (limit)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'sieve)
+;;; sieve.el ends here
+
