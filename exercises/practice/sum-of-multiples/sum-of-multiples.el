894151d55fb1e654230fc70e2093f5ae9c107821
Eric Willigers <keiravillekode@gmail.com>
2024-05-13 05:30:11 +1000

    Add sum-of-multiples exercise (#382)
    * Add sum-of-multiples exercise
    
    * empty stub

---
+++ exercises/practice/sum-of-multiples/sum-of-multiples.el
@@ -0,0 +1,13 @@
+;;; sum-of-multiples.el --- Sum of Multiples (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun sum (factors limit)
+ (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'sum-of-multiples)
+;;; sum-of-multiples.el ends here
