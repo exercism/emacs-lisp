d0662395cbeb2bf7414f19086e8610c18a2d097c
Eric Willigers <keiravillekode@gmail.com>
2024-05-15 06:44:36 +1000

    Add spiral-matrix exercise

---
+++ exercises/practice/spiral-matrix/spiral-matrix.el
@@ -0,0 +1,14 @@
+;;; spiral-matrix.el --- Spiral Matrix (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun spiral-matrix (size)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'spiral-matrix)
+;;; spiral-matrix.el ends here
+
