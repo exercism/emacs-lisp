e89a3411984eb69138a03319795d150a7110d18a
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-05-22 06:47:22 -0700

    Add dnd-character (#392)

---
+++ exercises/practice/dnd-character/.meta/example.el
@@ -0,0 +1,32 @@
+;;; dnd-character.el --- D&amp;D Character (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun modifier (score)
+  (floor (- score 10) 2))
+
+
+(defun ability ()
+  (let ((rolls '()))
+    (dotimes (i 4)
+      (push (+ 1 (random 6)) rolls))
+    (apply '+ (cdr (sort rolls '<)))))
+
+(defun generate-dnd-character ()
+   (let ((constitution (ability)))
+      (record 'dnd-character
+              (ability)                         ; strength
+              (ability)                         ; dexterity
+              constitution                      ; constitution
+              (ability)                         ; intelligence
+              (ability)                         ; wisdom
+              (ability)                         ; charisma
+              (+ 10 (modifier constitution))))) ; hitpoints
+
+
+(provide 'dnd-character)
+;;; dnd-character.el ends here
+
