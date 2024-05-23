2f055fb042d24643bc15f873ceec006b593d16fa
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-05-22 23:12:42 -0700

    Add robot simulator

---
+++ exercises/practice/robot-simulator/robot-simulator.el
@@ -0,0 +1,17 @@
+;;; robot-simulator.el --- robot-simulator (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun create-robot (x y direction)
+ (error "Delete this S-Expression and write your own implementation"))
+
+
+(defun move (robot instructions)
+ (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'robot-simulator)
+;;; robot-simulator.el ends here
