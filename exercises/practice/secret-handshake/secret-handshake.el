72f0bec5e12e13b4b918d22f7b9e0fd87116e048
Eric Willigers <keiravillekode@gmail.com>
2024-06-03 07:53:30 +1000

    Add secret-handshake exercise

---
+++ exercises/practice/secret-handshake/secret-handshake.el
@@ -0,0 +1,14 @@
+;;; secret-handshake.el --- Secret Handshake (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun commands (number)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'secret-handshake)
+;;; secret-handshake.el ends here
+
