e025d8859bac21280a459f033e049c47744f43f8
Eric Willigers <keiravillekode@gmail.com>
2024-06-04 18:21:48 +1000

    Add satellite exercise

---
+++ exercises/practice/satellite/satellite.el
@@ -0,0 +1,14 @@
+;;; satellite.el --- Satellite (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun tree-from-traversals (preorder inorder)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'satellite)
+;;; satellite.el ends here
+
