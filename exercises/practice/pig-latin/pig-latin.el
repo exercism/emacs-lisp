f82c51f7a6924f3b7e058b41cc838dc0e9cec67c
Kevin Marker <kmarker@gmail.com>
2024-06-10 08:59:48 -0500

    add Pig Latin, part of 24 in 48

---
+++ exercises/practice/pig-latin/pig-latin.el
@@ -0,0 +1,14 @@
+;;; pig-latin.el --- Pig Latin (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun translate (phrase)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'pig-latin)
+;;; pig-latin.el ends here
+
