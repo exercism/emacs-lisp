de6b742f8a71b3a88efe9205061595545dc605ce
Kevin Marker <kmarker@gmail.com>
2024-06-11 08:57:25 -0500

    add circular-buffer, part of 48 in 24

---
+++ exercises/practice/circular-buffer/circular-buffer.el
@@ -0,0 +1,13 @@
+;;; circular-buffer.el --- Circular Buffer (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defclass circular-buffer ()
+  (error "Delete this S-Expression and write your own implementation"))
+
+(provide 'circular-buffer)
+;;; circular-buffer.el ends here
+
