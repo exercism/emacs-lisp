47828ea5b373f96637b478d8185822865de0405e
Kevin Marker <kmarker@gmail.com>
2024-06-16 10:58:38 -0500

    add missing exercies for reverse-string #364

--- exercises/practice/reverse-string/.meta/example.el
+++ exercises/practice/reverse-string/.meta/example.el
@@ -5,9 +5,33 @@
 ;;; Code:
 
 
-(defun reverse-string (value)
-  (reverse value))
+(require 'cl-lib)
+(require 'ucs-normalize)
 
+(defun combining-mark-p (char)
+  (let ((category (get-char-code-property char 'general-category)))
+    (or (string= category "Mn")  ; Nonspacing_Mark
+        (string= category "Mc")  ; Spacing_Mark
+        (string= category "Me")))) ; Enclosing_Mark
+
+(defun split-grapheme-clusters (str)
+  (let ((clusters '())
+        (i 0)
+        (len (length str)))
+    (while (< i len)
+      (let ((start i)
+            (char (aref str i)))
+        (setq i (1+ i))
+        (while (and (< i len)
+                    (combining-mark-p (aref str i)))
+          (setq i (1+ i)))
+        (push (substring str start i) clusters)))
+    (nreverse clusters)))
+
+(defun reverse-string (value)
+  (let* ((decomposed (ucs-normalize-NFD-string value))
+         (clusters (split-grapheme-clusters decomposed)))
+    (apply #'concat (reverse clusters))))
 
 (provide 'reverse-string)
 ;;; reverse-string.el ends here
--- exercises/practice/reverse-string/reverse-string-test.el
+++ exercises/practice/reverse-string/reverse-string-test.el
@@ -32,6 +32,15 @@
 (ert-deftest an-even-sized-word ()
   (should (string= (reverse-string "drawer") "reward")))
 
+(ert-deftest wide-characters ()
+  (should (string= (reverse-string "子猫") "猫子")))
+
+(ert-deftest grapheme-cluster-with-pre-combined-form ()
+  (should (string= (reverse-string "Würstchenstand") "dnatsnehctsrüW")))
+
+(ert-deftest grapheme-cluster ()
+  (should (string= (reverse-string "ผู้เขียนโปรแกรม") "มรกแรปโนยขีเผู้")))
+
 
 (provide 'reverse-string-test)
 ;;; reverse-string-test.el ends here
