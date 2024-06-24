a0959567d3f087f6c86e07004e1cda451aca7f15
Kevin Marker <kmarker@gmail.com>
2024-06-24 10:38:46 -0500

    fix merge conflict

---
+++ exercises/practice/zebra-puzzle/zebra-puzzle.el
@@ -0,0 +1,18 @@
+;;; zebra-puzzle.el --- Zebra Puzzle (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun drinks-water ()
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(defun owns-zebra ()
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'zebra-puzzle)
+;;; zebra-puzzle.el ends here
+
