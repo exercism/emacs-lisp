682ca3ec9020e6b262a6799c8138c1343f649035
Kevin Marker <kmarker1101@users.noreply.github.com>
2024-06-25 15:03:51 -0500

    add strain exercise (#423)

---
+++ exercises/practice/strain/strain.el
@@ -0,0 +1,17 @@
+;;; strain.el --- Strain (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun keep (predicate list)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun discard (predicate list)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'strain)
+;;; strain.el ends here
+
