59658bc96fc229ce487c5bb73110421d9ed2c5cd
keiravillekode <keiravillekode@gmail.com>
2025-07-01 22:21:51 +1000

    Add house (#484)

---
+++ exercises/practice/house/house.el
@@ -0,0 +1,14 @@
+;;; house.el --- House (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun recite (start-verse end-verse)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'house)
+;;; house.el ends here
+
