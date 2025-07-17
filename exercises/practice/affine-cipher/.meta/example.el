6fb5080cd348824a4485a8e6d563b1d032889290
Eric Willigers <keiravillekode@gmail.com>
2025-07-18 06:49:28 +1000

    feedback

--- exercises/practice/affine-cipher/.meta/example.el
+++ exercises/practice/affine-cipher/.meta/example.el
@@ -48,7 +48,8 @@
     (+ (mod (* (mmi a m) (- (- char ?a) b)) m) ?a)))
 
 (defun mmi (a m)
-  "Find the modular multiplicative inverse (MMI) of A mod M."
+  "Find the modular multiplicative inverse (MMI) of A mod M
+using the extended Euclidean algorithm."
   (let ((tt 0) ;; t is already used as constant for truth
         (newtt 1)
         (r m)
