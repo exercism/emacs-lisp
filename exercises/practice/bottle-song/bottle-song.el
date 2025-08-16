069dbcddc66ab161a7428f1d82c2d0938ab85308
keiravillekode <keiravillekode@gmail.com>
2025-08-16 18:49:53 +1000

    Add bottle-song (#486)

---
+++ exercises/practice/bottle-song/bottle-song.el
@@ -0,0 +1,14 @@
+;;; bottle-song.el --- Bottle Song (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun recite (start-bottles take-down)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'bottle-song)
+;;; bottle-song.el ends here
+
