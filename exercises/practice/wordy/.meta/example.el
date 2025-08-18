07e726dfa44898f5d85023b9cf52d6ae894307ec
Eric Willigers <keiravillekode@gmail.com>
2025-08-18 18:05:49 +1000

    parse-error

--- exercises/practice/wordy/.meta/example.el
+++ exercises/practice/wordy/.meta/example.el
@@ -4,22 +4,26 @@
 
 ;;; Code:
 
+(define-error 'parse-error "unable to understand question")
+
 (defun calculate (left tokens)
   (if (null tokens)
     left
     (let ((operation (car tokens)))
       (cond
+        ((null (cdr tokens)) (signal 'parse-error nil))
         ((string= operation "plus") (calculate (+ left (string-to-number (cadr tokens))) (drop 2 tokens)))
         ((string= operation "minus") (calculate (- left (string-to-number (cadr tokens))) (drop 2 tokens)))
-        ((and (string= operation "multiplied") (string= (cadr tokens) "by")) (calculate (* left (string-to-number (caddr tokens))) (drop 3 tokens)))
-        ((and (string= operation "divided") (string= (cadr tokens) "by")) (calculate (/ left (string-to-number (caddr tokens))) (drop 3 tokens)))
-        (t (error "unknown operation"))))))
+        ((string= operation "multiplied-by") (calculate (* left (string-to-number (cadr tokens))) (drop 2 tokens)))
+        ((string= operation "divided-by") (calculate (/ left (string-to-number (cadr tokens))) (drop 2 tokens)))
+        (t (signal 'parse-error nil))))))
 
 (defun answer (question)
-  (unless (string= (substring question -1) "?") (error "syntax error"))
-  (let ((tokens (split-string (substring question 0 -1))))
-    (unless (string= (car tokens) "What") (error "unknown operation"))
-    (unless (string= (cadr tokens) "is") (error "unknown operation"))
+  (unless (string= (substring question -1) "?") (signal 'parse-error nil))
+  (let ((tokens (split-string (string-replace " by" "-by" (substring question 0 -1)))))
+    (unless (string= (car tokens) "What") (signal 'parse-error nil))
+    (unless (string= (cadr tokens) "is") (signal 'parse-error nil))
+    (unless (cddr tokens) (signal 'parse-error nil))
     (calculate (string-to-number (caddr tokens)) (drop 3 tokens))))
 
 
--- exercises/practice/wordy/wordy.el
+++ exercises/practice/wordy/wordy.el
@@ -5,6 +5,10 @@
 ;;; Code:
 
 
+(define-error 'parse-error
+              (error "Delete this S-Expression and write your own implementation"))
+
+
 (defun answer (question)
   (error "Delete this S-Expression and write your own implementation"))
 
