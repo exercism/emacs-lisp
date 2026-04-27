ec5634653310d7e1120ac9623ed55814459336d9
keiravillekode <keiravillekode@gmail.com>
2026-04-28 06:29:53 +1000

    Add `rational-numbers` exercise (#517)

---
+++ exercises/practice/rational-numbers/.meta/example.el
@@ -0,0 +1,46 @@
+;;; rational-numbers.el --- Rational Numbers (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun reducerational (r)
+  (cl-labels ((rat-gcd (a b)
+                (if (= b 0) (abs a)
+                  (rat-gcd b (% a b)))))
+    (let* ((a (car r))
+           (b (cdr r))
+           (g (rat-gcd a b))
+           (c (if (< b 0) (- g) g)))
+      (cons (/ a c) (/ b c)))))
+
+(defun add (r1 r2)
+  (reducerational (cons (+ (* (car r1) (cdr r2)) (* (car r2) (cdr r1)))
+                        (* (cdr r1) (cdr r2)))))
+
+(defun sub (r1 r2)
+  (add r1 (cons (- (car r2)) (cdr r2))))
+
+(defun mul (r1 r2)
+  (reducerational (cons (* (car r1) (car r2))
+                        (* (cdr r1) (cdr r2)))))
+
+(defun div (r1 r2)
+  (mul r1 (cons (cdr r2) (car r2))))
+
+(defun absrational (r)
+  (reducerational (cons (abs (car r)) (abs (cdr r)))))
+
+(defun exprational (r n)
+  (let ((r (reducerational r)))
+    (if (< n 0)
+        (exprational (cons (cdr r) (car r)) (- n))
+      (cons (expt (car r) n) (expt (cdr r) n)))))
+
+(defun expreal (x r)
+  (exp (* (car r) (/ (log x) (cdr r)))))
+
+
+(provide 'rational-numbers)
+;;; rational-numbers.el ends here
---
+++ exercises/practice/rational-numbers/rational-numbers.el
@@ -0,0 +1,34 @@
+;;; rational-numbers.el --- Rational Numbers (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun reducerational (r)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun add (r1 r2)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun sub (r1 r2)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun mul (r1 r2)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun div (r1 r2)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun absrational (r)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun exprational (r n)
+  (error "Delete this S-Expression and write your own implementation"))
+
+(defun expreal (x r)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'rational-numbers)
+;;; rational-numbers.el ends here
