2b37d664e9730bc2c8cbd1be57e90271a24a7313
Eric Willigers <keiravillekode@gmail.com>
2026-08-15 19:28:38 +1000

    piecing-it-together

---
+++ exercises/practice/piecing-it-together/piecing-it-together.el
@@ -0,0 +1,14 @@
+;;; piecing-it-together.el --- Piecing It Together (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun jigsaw-data (partial)
+  (error "Delete this S-Expression and write your own implementation"))
+
+
+(provide 'piecing-it-together)
+;;; piecing-it-together.el ends here
+
